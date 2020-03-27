2eac3a56e07d1e84ebdab68916fc0b5e10afbf4f
wolfwithcode <wolfwithcode@gmail.com>
2020-03-27 17:30:20 +0700

    backUp lesson 9 update button

--- src/App.js
+++ src/App.js
@@ -7,24 +7,30 @@ import data from './sampleData'
 class App extends React.Component {
   state = {
     // boardTitle: 'house ideas'
-    board: []
+    boards: []
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.setState( { boards : data.boards });
   }
 
-  updateState = () => {
-    this.setState( { boards : data.boards });
-    // this.state.boards = "some value";
-  }
+  // updateState = () => {
+  //   this.setState( { boards : data.boards });
+  //   // this.state.boards = "some value";
+  // }
 
   render(){
+    // this.setState( { boards : data.boards });
+    // console.log(this.state.boards);
     return (
       <div>        
-        <button onClick={this.updateState}></button>
-        {console.table(this.state.boards)}
-        <p>{this.state.boardTitle}</p>        
+        {this.state.boards.map(board => (
+          <div key={board.id}>
+            <span>{board.id}</span>
+            <h3>{board.title}</h3>
+            <p>{board.background}</p>
+          </div>
+        ))}      
       </div>
     );
   }
