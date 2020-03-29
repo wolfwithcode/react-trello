6a44940b8b022491c6a1a8b76b83e3fb8d4aaa54
wolfwithcode <wolfwithcode@gmail.com>
2020-03-29 17:44:25 +0700

    Back up lesson 19

--- src/components/BoardPreview.js
+++ src/components/BoardPreview.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import Board from './Board'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+
 
 class BoardPreview extends React.Component {
+    
+    goToBoard = () => {
+        const boardId = this.props.board.id
+        this.props.history.push({
+            pathname: `/board/${boardId}`
+        })
+    }
+
     render() {
         return (
-            <p>{this.props.board.title}</p>
+            <ul
+            className="board-preview-item"
+            onClick={this.goToBoard}
+            style={{ backgroundColor:  this.props.board.background }}
+            >
+                <li>{ this.props.board.title }</li>
+            </ul>
         )
     }
 }
@@ -15,4 +31,4 @@ BoardPreview.propTypes = {
 }
 
 
-export default BoardPreview
+export default withRouter(BoardPreview)
