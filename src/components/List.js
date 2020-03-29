d3d69b79ee3261016a7872a9a2a9db4f0f75a28b
wolfwithcode <wolfwithcode@gmail.com>
2020-03-29 16:16:29 +0700

    Back up lesson 15

--- src/components/List.js
+++ src/components/List.js
@@ -4,6 +4,28 @@ import { render } from '@testing-library/react'
 import PropTypes from 'prop-types'
 
 class List extends React.Component {
+    state = {
+        currentCards: []
+    }
+    nameInput = React.createRef();
+    createNewCard = (e) => {
+        e.preventDefault();
+        const card = {
+            text: this.nameInput.current.value,
+            listId: 'abc123',
+            labels: [],
+            createdAt: new Date()
+        }
+
+        if(card.text){
+            this.setState(
+                { currentCards: 
+                    [...this.state.currentCards, card]
+                })
+        }
+        this.nameInput.current.value = ''
+        console.log('new card added '+ card.text)
+    }
     render(){
         return (
             <div className="list" >
@@ -16,8 +38,17 @@ class List extends React.Component {
                     data={this.props.list.cards[key]}
                   />  
                 ))}
-                
-                           
+                <form 
+                    onSubmit={this.createNewCard} 
+                    className="new-card-wrapper" >
+                    <input 
+                        type="text"
+                        ref={this.nameInput}
+                        name="name"
+                        placeholder=" + New card"
+                    />
+                </form>
+
             </div>
            
         )
