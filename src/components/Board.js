0738527c6269f03b0f607d0024f3d2257336c9d4
wolfwithcode <wolfwithcode@gmail.com>
2020-03-29 15:55:55 +0700

    Back up lesson 14

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,26 +11,59 @@ class Board extends React.Component {
         this.setState({ currentLists: data.lists })
     }
 
-    createNewList = () => {
+    addBoardInput = React.createRef();
+
+    createNewList = (e) => {
+        e.preventDefault();
+        // console.log(this.addBoardInput.current.value);
         const list = {
             id: Math.random(),
-            title: "My amazing list",
+            title: this.addBoardInput.current.value,
             board: 300,
-            createdAt: new Date()
+            createdAt: new Date(),
+            cards: [
+                {
+                    id: 1,
+                    text: 'Card 1'
+                },
+                {
+                    id: 2,
+                    text: 'Card 2'
+                }
+               
+            ]
         }
-        this.setState({currentLists: [...this.state.currentLists, list] })
+
+        if(list.title){
+            this.setState({currentLists: [...this.state.currentLists, list] });
+        }
+
+        this.addBoardInput.current.value = '';
+        
     }
 
     render(){
         return (
-            <div className="lists-wrapper">
-                <button onClick={this.createNewList}>New list</button>
-                {Object.keys(this.state.currentLists).map(key => (
-                    <List 
-                    key={ this.state.currentLists[key].id }
-                    list={ this.state.currentLists[key] }
-                    />
-                ))}              
+            <div>
+                <div className="lists-wrapper">
+                    <button onClick={this.createNewList}>New list</button>
+                    {Object.keys(this.state.currentLists).map(key => (
+                        <List 
+                        key={ this.state.currentLists[key].id }
+                        list={ this.state.currentLists[key] }
+                        />
+                    ))}              
+                </div>
+
+                <form onSubmit={this.createNewList}
+                        className="new-list-wrapper">
+                        <input
+                            type="text"
+                            ref={this.addBoardInput}
+                            name="name"
+                            placeholder=" + New List" />
+
+                </form>
             </div>
             
         )
