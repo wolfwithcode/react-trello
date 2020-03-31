641ba43817e593c59ee1b5355ec651d6b82e77ef
wolfwithcode <wolfwithcode@gmail.com>
2020-03-31 16:00:00 +0700

    Back up lesson 37

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,17 +1,36 @@
 import React from 'react'
+import { firebaseAuth } from '../firebase';
 
 const AuthContext = React.createContext()
 
 class AuthProvider extends React.Component {
+    
     state = {
         user: {
             name: 'Chris'
         }
     }
+
+    signUp = async (email, password, e) => {
+        try {
+            e.preventDefault();
+            await firebaseAuth.createUserWithEmailAndPassword(
+                email,
+                password
+            )
+        } catch(error) {
+
+        }
+    }
+
     render() {
         return (
             <AuthContext.Provider
-                value={{ user: this.state.user }}>
+                value={{ 
+                    user: this.state.user,
+                    signUp: this.signUp 
+                }}>
+                    
                 {this.props.children}
 
             </AuthContext.Provider>
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AuthConsumer } from './AuthContext'
 
 class UserForm extends React.Component {
     emailInput = React.createRef()
@@ -6,35 +7,44 @@ class UserForm extends React.Component {
 
     render() {
         return (
-            <React.Fragment>
-                <div className="sign-up-wrapper">
-                    <h2>Sign in or create account</h2>
-                    <form className="sign-up-form">
-                        <div>
-                            <input 
-                                ref={this.emailInput}
-                                type="email"
-                                name="email"
-                                placeholder="Email"
-                            />
-                        </div>
-                        <div>
-                            <input 
-                                ref={this.passwordInput}
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-                            />
-                        </div>
-                        <div>
-                            <button>
-                                Sign Up
-                            </button>
-                        </div>                        
-                    </form>                    
-                </div>
-
-            </React.Fragment>
+            <AuthConsumer>
+                {({ signUp }) => (
+                    <React.Fragment>
+                    <div className="sign-up-wrapper">
+                        <h2>Sign in or create account</h2>
+                        <form className="sign-up-form">
+                            <div>
+                                <input 
+                                    ref={this.emailInput}
+                                    type="email"
+                                    name="email"
+                                    placeholder="Email"
+                                />
+                            </div>
+                            <div>
+                                <input 
+                                    ref={this.passwordInput}
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <div>
+                                <button
+                                onClick={(e) => signUp(
+                                    this.emailInput.current.value,
+                                    this.passwordInput.current.value,
+                                    e
+                                )}>
+                                    Sign Up
+                                </button>
+                            </div>                        
+                        </form>                    
+                    </div>
+                    </React.Fragment>
+                ) }
+            </AuthConsumer>
+            
         )
     }
 }
